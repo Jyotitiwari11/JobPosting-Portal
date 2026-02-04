143a3e35604970c9f0c74151c37c5b198913cd7b
Jyotitiwari <tiwarijyoti11112002>
2026-02-04 23:32:56 +0530

    api

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBNs-FVSz6NqtX48gdlmo91KKCmeiwqIb8",
-  authDomain: "pilot-app-7d731.firebaseapp.com",
-  projectId: "pilot-app-7d731",
-  storageBucket: "pilot-app-7d731.firebasestorage.app",
-  messagingSenderId: "881189188830",
-  appId: "1:881189188830:web:6f97dbbc5b72a562cefe4f"
-};
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
